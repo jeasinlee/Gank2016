f44c7fd22156decf19cbaf31ca1a53ab5c432ab0
Jixiang_Li <jeasinlee@163.com>
2016-12-05 17:34:32 +0800

    v2.23//集成db，耽误太多时间，因为realm版本问题

--- android/app/src/main/java/com/gank2016/MainActivity.java
+++ android/app/src/main/java/com/gank2016/MainActivity.java
@@ -1,6 +1,7 @@
 package com.gank2016;
 
 import com.facebook.react.ReactActivity;
+import io.realm.react.RealmReactPackage;
 
 public class MainActivity extends ReactActivity {
 
--- android/app/src/main/java/com/gank2016/MainApplication.java
+++ android/app/src/main/java/com/gank2016/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.react.rnspinkit.RNSpinkitPackage;
+import io.realm.react.RealmReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),new RNSpinkitPackage()
+          new MainReactPackage(),new RNSpinkitPackage(),new RealmReactPackage()
       );
     }
   };
